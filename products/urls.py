3adb17dc54b7960b0bc5737862e3f751f951e128
wesley-ruede <wesley.k.r1993@gmail.com>
2019-06-17 21:36:49 -0400

    website finished

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views #home page view will be based in products
 urlpatterns = [
 	path('create', views.create, name='create'), #defined url reference from main urls.py to views.py
 	path('<int:product_id>', views.detail, name='detail'), #product detail path
+	path('<int:product_id>/upvote', views.upvote, name='upvote'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,9 +6,10 @@ from django.utils import timezone
 # home page views will be referenced here
 
 def home(request): # home function with request so forwared http requests can be served
-	return render(request, 'products/home.html') #render the requested html page located in products/templates/products
+    products = Product.objects
+    return render(request, 'products/home.html',{'products':products}) #render the requested html page located in products/templates/products
 
-@login_required #decorator to impose login required
+@login_required(login_url="/accounts/signup") #decorator to impose login and send user to signup page if they are not logged in
 def create(request):
     if request.method == 'POST': #must POST GET is unsafe
         if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']: #all fields required
@@ -30,6 +31,16 @@ def create(request):
     else:
         return render(request, 'products/create.html') #if all else fails send user back to create.html
 
+
 def detail(request, product_id): #int product id
 	product = get_object_or_404(Product, pk=product_id) # get the Product object based on int product_id or deliver 404
 	return render(request, 'products/detail.html',{'product':product}) #forward user to details.html
+
+
+@login_required(login_url="/accounts/signup") #decorator to impose login and send user to signup page if they are not logged in
+def upvote(request, product_id): #int product id
+    if request.method == 'POST': #must POST GET is unsafe
+        product = get_object_or_404(Product, pk=product_id) # get the Product object based on int product_id or deliver 404
+        product.vote_totals += 1 #increment vote_totals
+        product.save() #update product
+        return redirect('/products/' + str(product.id)) #forward user to products urls based on int product id
